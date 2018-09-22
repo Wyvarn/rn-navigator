280baae600551817465591bc183573f0b3481080
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2018-09-22 22:16:54 +0300

    feat: buildSceneConfig
    This will add a build scene configuration that will build the stack to be used by navigator,
    providing each object with a key and component where the key is the name of the current scene and
    component is the actual component in the stack.wq

--- Navigator.js
+++ Navigator.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { arrayOf, object } from 'prop-types';
 
+const buildSceneConfig = (children = []) => {
+    const config = {};
+
+    children.forEach(child => {
+        const { name, component } = child;
+        config[name] = { key: name, component};
+    });
+
+    return config
+}
 
 export const Route = () => null
 
 export class Navigator extends Component {
-    render() {
-        return null;
+    constructor(props){
+        super(props);
+
+        const sceneConfig = buildSceneConfig(props.children);
+        const initialSceneName = props.children[0].props.name;
+
+        this.state = {
+            sceneConfig,
+            stack: [sceneConfig[initialSceneName]]
+        }
     }
+    
+    render() {
+        const CurrentScene = this.state.stack[0].component;
+        return <CurrentScene />;
+    }
+}
+
+Navigator.propTypes = {
+    children: arrayOf(object)
 }
