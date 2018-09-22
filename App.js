ae323b992ae44a2ba16a37d28a508c8acdd9e718
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2018-09-22 22:01:43 +0300

    feat: default navigator
    starting point for navigator

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
+import { Route, Navigator } from './Navigator';
+
+const Screen1 = ({ navigator }) => (
+  <View style={[styles.screen, { backgroundColor: '#59C9A5' }]}>
+    <Button
+      title="Screen 2"
+      onPress={() => navigator.push('Screen2')}
+    />
+    <Button
+      title="Pop"
+      onPress={() => navigator.pop()}
+    />
+  </View>
+);
+
+const Screen2 = ({ navigator }) => (
+  <View style={[styles.screen, { backgroundColor: '#23395B' }]}>
+    <Button
+      title="Screen 3"
+      onPress={() => navigator.push('Screen3')}
+    />
+    <Button
+      title="Pop"
+      onPress={() => navigator.pop()}
+    />
+  </View>
+);
+
+const Screen3 = ({ navigator }) => (
+  <View style={[styles.screen, { backgroundColor: '#B9E3C6' }]}>
+    <Button
+      title="Pop"
+      onPress={() => navigator.pop()}
+    />
+  </View>
+);
+
+export default Screen1;
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+
+      <Navigator>
+        <Route name="Screen1" component={Screen1} />
+        <Route name="Screen2" component={Screen2} />
+        <Route name="Screen3" component={Screen3} />
+      </Navigator>
     );
   }
 }
@@ -14,7 +55,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Navigator.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export const Route = () => null
+
+export class Navigator extends Component {
+    render() {
+        return null;
+    }
+}
